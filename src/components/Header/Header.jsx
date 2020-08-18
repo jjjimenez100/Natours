4bd0a1e6684844efa1e72d8237c897e4eff92483
jjjimenez100 <jimenez.johnjoshua.jjj@gmail.com>
2020-08-18 22:47:49 +0800

    Finish section 1 of natours project

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { 
+import {
     header,
     logo,
     logoContainer,
@@ -7,13 +7,16 @@ import {
     primaryHeadingMain,
     primaryHeadingSub,
     contentContainer,
+    btn,
+    btnWhite,
+    btnAnimated,
 } from './Header.module.css';
 
-export default function Header () {
+export default function Header() {
     return (
         <header className={header}>
             <div className={logoContainer}>
-                <img src="/images/logo-white.png" alt="Logo" className={logo}/>
+                <img src="/images/logo-white.png" alt="Logo" className={logo} />
             </div>
 
             <div className={contentContainer}>
@@ -21,6 +24,8 @@ export default function Header () {
                     <span className={primaryHeadingMain}>Outdoors</span>
                     <span className={primaryHeadingSub}>is where life happens</span>
                 </h1>
+
+                <a href="#" className={`${btn} ${btnWhite} ${btnAnimated}`}>Discover our tours</a>
             </div>
         </header>
     )
